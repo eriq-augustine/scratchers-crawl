d0bba78bbb3a3c5fcd707046f55d1e10b532bf75
Eriq Augustine <eriq.augustine@gmail.com>
2026-09-24 09:11:04 -0500

    Working on CI.

--- scripts/crawl_upload.sh
+++ scripts/crawl_upload.sh
@@ -18,27 +18,39 @@ function main() {
 
     local datestamp=$(date -u +%Y-%m-%dT%TZ)
 
-    git clone git@github.com:eriq-augustine/scratchers.git
+    local code_url='git@github.com:eriq-augustine/scratchers.git'
+    if [[ -v SCRATCHERS_PAT ]] ; then
+        code_url="https://eriq-augustine:${SCRATCHERS_PAT}@github.com/eriq-augustine/scratchers.git"
+    fi
+
+    echo "Cloning Repos"
+    git clone "${code_url}"
     git clone git@github.com:eriq-augustine/scratchers-data.git
 
     cd scratchers
 
+    echo "Installing Dependencies"
     python3 -m venv venv
     source ./venv/bin/activate
     pip install -r requirements.txt -r requirements-dev.txt
 
+
+    echo "Crawling"
     python3 -m scratchers.cli.crawl | gzip > data.json.gz
 
+    echo "Computing Stats"
     python3 -m scratchers.cli.compute-game-stats data.json.gz --simulation --update
 
     cp data.json.gz "../scratchers-data/data/${datestamp}.json.gz"
 
+    echo "Building Site"
     rm -r ../scratchers-data/docs
     python3 -m scratchers.cli.build-site ../scratchers-data/data --out-dir ../scratchers-data/docs
 
+    echo "Deploying"
     cd ../scratchers-data
     git add .
-    git commit -m "Updated data." --author "Scratchers Crawl <s-crawl@eriqaugustine.com>"
+    git -c user.email='s-crawl@eriqaugustine.com' -c user.name='Scratchers Crawl' commit -m "Updated data."
     git push
 
     return 0
